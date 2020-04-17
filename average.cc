6aa00c877a3f805d7c52713e0d3eb3820f0d17dd
Triantafyllidis Theocharis <triantt1@emu.org.aalto.fi>
2020-04-17 11:28:18 +0300

    done

--- average.cc
+++ average.cc
@@ -1,8 +1,28 @@
+
 #include "average.h"
 
 Result calculate(int ny, int nx, const float *data, int y0, int x0, int y1,
-                 int x1) {
-  // Your implementation here
-  Result result{{1.0f, 0.0f, 1.0f}};
-  return result;
+                 int x1)
+ {
+	int area=(x1-x0)*(y1-y0);
+	double value[3]={0,0,0};
+	for (int c=0;c<3;c++)
+		{
+		for (int x=x0;x<nx;x++)
+		       	{
+			for (int y=y0;y<ny;y++)
+ 				{ 
+					value[c]=value[c]+data[c+3*x+3*nx*y];
+                        	  }
+		         }
+ 		 value[c]=value[c]/area;
+	 }
+//Result result{{1.0f, 0.0f, 1.0f}};
+Result result {{ float(value[0]),float(value[1]),float (value[2])}};
+result.avg[0]= value[0];
+result.avg[1]=value[1];
+result.avg[2]=value[2];
+ return result;
+
 }
+
